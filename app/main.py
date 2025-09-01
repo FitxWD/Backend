a7152cb07b4b4a05e6d39ed96da5c6e5013fcb70
ChehanHelitha <162694727+ChehanHelitha@users.noreply.github.com>
2025-09-01 15:15:38 +0530

    Connecting backend to frontend and database

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import users  
 
 app = FastAPI(title="Wellness Assistant API")
+
+origins = [
+    "http://localhost:3000",  # Your Next.js frontend
+]
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include router
 app.include_router(users.router, prefix="/api/v1", tags=["users"])
